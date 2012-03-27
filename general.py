cf53b2a35c592de44e05099917de0d549d1cf43d
Nic Eggert <nse23@cornell.edu>
2012-03-27 11:00:34 -0400

    Update weights for 5/fb

--- general.py
+++ general.py
@@ -9,25 +9,25 @@ from cmsutilities import get_TLorentzVector, get_PF_isolation, angle_0_2pi, get_
 
 lm6_files = ["/Users/nic/cms/SUSY/Data/LM6/TopDilLikeSelection_LM6_v3.root"]
 lm6_hd5 = "Data/lm6.hdf5"
-lm6_w = 0.00142321668061303
+lm6_w = 0.00355804170153257
 tt_files = ["/Users/nic/cms/SUSY/Data/TTJets/TopDilLikeSelection_ttjets_v3.root"]
 tt_hd5 = "Data/tt.hdf5"
-tt_w = 0.0891422810753368
+tt_w = 0.222855702688342
 wjets_files = ["/Users/nic/cms/SUSY/Data/WJets/TopDilLikeSelection_Wjets_v3.root"]
 wjets_hd5 = "Data/wjets.hdf5"
-wjets_w = 0.769834210914587
+wjets_w = 1.92458552728647
 qcd1_files = ["/Users/nic/cms/SUSY/Data/QCD/TopDilLikeSelection_QCD-HT-100-250_v3.root"]
 qcd1_hd5 = "Data/qcd100-250.hdf5"
-qcd1_w = 589.746755581942
+qcd1_w = 1474.36688895486
 qcd2_files = ["/Users/nic/cms/SUSY/Data/QCD/TopDilLikeSelection_QCD-HT-250-500_v3.root"]
 qcd2_hd5 = "Data/qcd250-500.hdf5"
-qcd2_w = 16.5423419380437
+qcd2_w = 41.3558548451093
 qcd3_files = ["/Users/nic/cms/SUSY/Data/QCD/TopDilLikeSelection_QCD-HT-500-1000_v3.root"]
 qcd3_hd5 = "Data/qcd500-1000.hdf5"
-qcd3_w = 0.720347867067282
+qcd3_w = 1.8008696676682
 qcd4_files = ["/Users/nic/cms/SUSY/Data/QCD/TopDilLikeSelection_QCD-HT-1000_v3.root"]
 qcd4_hd5 = "Data/qcd1000.hdf5"
-qcd4_w = 0.0263707592125691
+qcd4_w = 0.0659268980314228
 
 fake_data = {'delta': 1.5086990594863892,
  'phiUp': 5.9232745243905569,

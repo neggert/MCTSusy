f31965b9877c6decf9eaa59c7e2930aadfc59f20
Nic Eggert <nse23@cornell.edu>
2012-03-29 16:44:28 -0400

    Move root plotting functions in to CMSPyLibs

--- plots.py
+++ plots.py
@@ -1,5 +1,7 @@
 from general import *
 from math import sqrt
+import ROOT
+from CMSPyLibs.rootplot import *
 
 ROOT.gStyle.SetPalette(1)
 
@@ -40,33 +42,6 @@ def plot_stacked_mct_hist(bins=20, histrange=(0,300)) :
     l.AddEntry("WJets", "WJets", "f")
     l.SetFillColor(ROOT.kWhite)
     l.Draw()
-    raw_input("...")
-
-def plot_hist_ROOT( data, bins=20, range=(0., 100.), name="hist") :
-    """Plot a TH1D of data"""
-    hist = ROOT.TH1D(name, name, bins, range[0], range[1])
-    for d in data :
-        hist.Fill(d)
-    hist.SetMinimum(0.)
-    hist.Draw()
-
-    return hist
-
-def plot_hist2d_ROOT( xdata, ydata, xbins=20, xrange=(0., 100.), ybins=20, yrange=(0., 100.), name="hist2d") :
-    """Plot a TH2D of data"""
-    hist = ROOT.TH2D(name, name, xbins, xrange[0], xrange[1], ybins, yrange[0], yrange[1])
-    for x,y in zip(xdata,ydata) :
-        hist.Fill(x,y)
-    hist.Draw("COLZ")
-
-    return hist
-
-def plot_graph_ROOT( x, y ) :
-    """Plot a TGraph of x vs y"""
-    g = ROOT.TGraph(len(x), array("f", x), array("f", y))
-    g.SetMarkerStyle(7)
-    # g.SetMarkerSize(4)
-    g.Draw("pa")
     raw_input("...")
 
 def plot_data_hist( hdf_file, data_item, bins=30, range=(0,300)) :

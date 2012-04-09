dce06e94086ab5f05f047758f7bfa2efea614853
Nic Eggert <nse23@cornell.edu>
2012-04-09 14:23:58 -0400

    Remove old data handling and add function for saving to pandas dataframe

--- data_handling.py
+++ data_handling.py
@@ -1,4 +1,4 @@
-import tables
+import pandas
 import pylab
 import ROOT
 import itertools
@@ -15,142 +15,66 @@ Includes creating the files and different ways to iterate over data
 in a file.
 """
 
-class FitEvent(tables.IsDescription):
-    idnumber = tables.Int64Col()
-    pdg1     = tables.Int64Col()
-    pdg2     = tables.Int64Col()
-    pt1      = tables.Float64Col()
-    pt2      = tables.Float64Col()
-    met      = tables.Float64Col()
-    delta    = tables.Float64Col()
-    phiUp    = tables.Float64Col()
-    sPhiUp   = tables.Float64Col()
-    relIso1  = tables.Float64Col()
-    relIso2  = tables.Float64Col()
-    mct      = tables.Float64Col()
-    mct_type = tables.Int64Col()
-    njets    = tables.Int64Col()
-    nbjets   = tables.Int64Col()
-    mctype   = tables.StringCol(8)
-
-def save_data_hdf5( input_files, output_file, mctype="mc" ):
-    """Load all of the relevant data from the CMSSW files and save it using pyTables"""
+def save_data_pandas( input_files, output_file, mctype="mc", weight=1.):
+    """Load all of the relevant data from the CMSSW files and save it using pandas"""
     getter = dilepton_event.CMSDileptonEventGetter(input_files)
     getter.set_electron_collection("loosePatElectrons")
     getter.set_muon_collection("loosePatMuons")
 
-    h5file = tables.openFile(output_file, "w", "Fit Data File")
-    group = h5file.createGroup("/", "fit", "Fit information")
-
-    table = h5file.createTable( group, "fitdata", FitEvent, "Fit Data")
-
     count = event_counter.EventCounter()
     calc = general_calc.VarCalculator()
 
-    try :
-        for event in getter.events() :
-            event.jet_btag = "combinedSecondaryVertexBJetTags"
-            event.jet_btag_cut = 0.244
-            datarow = table.row
-            datarow["idnumber"] = count.eventNum
-            datarow["mctype"] = mctype
-            count.Increment()
+    datadicts = []
 
-            # lepton info
-            leptons = event.get_leptons()
-            leptons.sort(key = get_PF_isolation)
-            datarow["relIso1"] = get_PF_isolation( leptons[0])
-            datarow["relIso2"] = get_PF_isolation( leptons[1])
-            datarow["pt1"] = leptons[0].pt()
-            datarow["pt2"] = leptons[1].pt()
-            datarow["pdg1"] = leptons[0].pdgId()
-            datarow["pdg2"] = leptons[1].pdgId()
+    for event in getter.events() :
+        event.jet_btag = "combinedSecondaryVertexBJetTags"
+        event.jet_btag_cut = 0.244
+        datarow = {}
+        datarow["mctype"] = mctype
+        count.Increment()
 
-            # jets
-            datarow['njets'] = len(event.get_jets())
-            datarow['nbjets'] = len(event.get_tagged_jets())
+        # lepton info
+        leptons = event.get_leptons()
+        leptons.sort(key = get_PF_isolation)
+        datarow["relIso1"] = get_PF_isolation( leptons[0])
+        datarow["relIso2"] = get_PF_isolation( leptons[1])
+        datarow["pt1"] = leptons[0].pt()
+        datarow["pt2"] = leptons[1].pt()
+        datarow["pdg1"] = leptons[0].pdgId()
+        datarow["pdg2"] = leptons[1].pdgId()
+        datarow["phi1"] = leptons[0].phi()
+        datarow["phi2"] = leptons[1].phi()
 
-            # angles
-            phi1 = leptons[0].phi()
-            phi2 = leptons[1].phi()
-            phiUp = event.upstream_of_leptons().Phi()
-            phiUpSigMatrix = event.get_met().getSignificanceMatrix()
-            gamma = angle_0_2pi((phi1+phi2)/2) # average of visible angles
-            twodelta = abs(phi1-phi2)
-            if twodelta > pylab.pi : twodelta = 2*pylab.pi-twodelta
-            datarow["delta"] = twodelta/2
-            datarow["phiUp"] = angle_0_2pi(phiUp - gamma)
-            datarow["sPhiUp"] = get_upstream_phi_res(event.upstream_of_leptons(), phiUpSigMatrix)
+        # jets
+        datarow['njets'] = len(event.get_jets())
+        datarow['nbjets'] = len(event.get_tagged_jets())
 
-            # mct
-            p4jet1 = ROOT.TLorentzVector(0., 0., 0., 0.)
-            p4jet2 = ROOT.TLorentzVector(0., 0., 0., 0.)
-            met = event.get_met()
-            calc.setP4s(p4jet1, p4jet2, get_TLorentzVector(leptons[0]), get_TLorentzVector(leptons[1]), get_TLorentzVector(met))
-            datarow["mct"], datarow['mct_type'] = calc.mctBoostCor_210()
+        # angles
+        phi1 = leptons[0].phi()
+        phi2 = leptons[1].phi()
+        phiUp = event.upstream_of_leptons().Phi()
+        phiUpSigMatrix = event.get_met().getSignificanceMatrix()
+        gamma = angle_0_2pi((phi1+phi2)/2) # average of visible angles
+        twodelta = abs(phi1-phi2)
+        if twodelta > pylab.pi : twodelta = 2*pylab.pi-twodelta
+        datarow["delta"] = twodelta/2
+        datarow["phiUp"] = angle_0_2pi(phiUp - gamma)
+        datarow["sPhiUp"] = get_upstream_phi_res(event.upstream_of_leptons(), phiUpSigMatrix)
 
-            datarow["met"] = event.get_met().pt()
+        # mct
+        p4jet1 = ROOT.TLorentzVector(0., 0., 0., 0.)
+        p4jet2 = ROOT.TLorentzVector(0., 0., 0., 0.)
+        met = event.get_met()
+        calc.setP4s(p4jet1, p4jet2, get_TLorentzVector(leptons[0]), get_TLorentzVector(leptons[1]), get_TLorentzVector(met))
+        datarow["mct"], datarow['mct_type'] = calc.mctBoostCor_210()
 
-            datarow.append()
-    finally : 
-        table.flush()
-        h5file.close()
+        # met
+        datarow["metPt"] = event.get_met().pt()
+        datarow['metPhi'] = event.get_met().phi()
+        datarow['weight'] = weight
 
-def load_data_iterator( hd5_file ) :
-    """Load the fit data from the pytables file"""
-    f = tables.openFile(hd5_file, "r")
-    table = f.root.fit.fitdata
-    return table.iterrows()
+        datadicts.append(datarow)
 
-def load_data_iterator_cut( hd5_file, cut ) :
-    """Load the fit data from the pytables file with a cut specified by the string cut"""
-    f = tables.openFile(hd5_file, "r")
-    table = f.root.fit.fitdata
-    return table.where(cut)
-
-def load_data_iterator_iso( hd5_file ) :
-    """Load the fit data from the pickle file with an isolation cut"""
-    f = tables.openFile(hd5_file, "r")
-    table = f.root.fit.fitdata
-    isocut = """((relIso1 < 0.17 and abs(pdg1) ==11) or (relIso1 < 0.2 and abs(pdg1) == 13)) and ((relIso2 < 0.17 and abs(pdg2) ==11) or (relIso2 < 0.2 and abs(pdg2) == 13))"""
-    return table.where(isocut)
-
-def get_expected_events( hd5_file, weight) :
-    f = tables.openFile(hd5_file, "r")
-    table = f.root.fit.fitdata
-    return table.nrows*weight
-
-class random_sample (object) :
-    """A random sample (with replacement) of events"""
-    def __init__(self, hd5_files, weights):
-        """Intialize with a list of hd5_files"""
-        self.hd5_files = hd5_files
-        self.files = [tables.openFile(hd5_file, "r") for hd5_file in hd5_files]
-        self.tables = [f.root.fit.fitdata for f in self.files]
-        self.weights = weights
-        self.eventlists = self.resample()
-
-    def resample(self) :
-        """Sample event numbers with replacement"""
-        eventlists = []
-        for i, t in enumerate(self.tables) :
-            nevents_total = t.nrows
-            nevents_select = numpy.random.poisson(t.nrows*self.weights[i])
-            # print nevents_select, "events for sample", self.hd5_files[i]
-            eventlists.append(numpy.random.choice(range(nevents_total), nevents_select, replace=True))
-        return eventlists
-    
-    def sampled_iter(self, sequence, eventlist) :
-        for event in eventlist:
-            yield sequence[event]
-
-    def __call__(self ) :
-        """Make a copy of the iterator."""
-        iterators = [[d for d in t.read() ] for t in self.tables]
-        filtered_iterators = [self.sampled_iter(i, l) for l,i in zip(self.eventlists, iterators)]
-        return itertools.chain(*filtered_iterators)
-
-
-
-
-
+    store = pandas.HDFStore(output_file)
+    df = pandas.DataFrame(datadicts)
+    store[mctype] = df

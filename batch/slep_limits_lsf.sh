f1ec12cae09e9599f652812fa0fbcc1fbc745f35
Nic Eggert <nic.eggert@gmail.com>
2013-01-29 10:04:02 -0500

    Add slepton limit script

--- batch/slep_limits_lsf.sh
+++ batch/slep_limits_lsf.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-#BSUB -J "chi_limits[1-510]"
+#BSUB -J "slep_limits[1-500]"
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
-#BSUB -q 2nw
+#BSUB -q 1nw
 
 echo $LSB_JOBINDEX
 
@@ -16,4 +16,4 @@ pythonbrew use 2.7.3
 
 
 cd $HOME/work/MCTSusy
-pythonbrew py -p 2.7.3 set_limits.py batch sig_chi.root chi_masses.json $(($LSB_JOBINDEX-1)) limits/results/chi_$LSB_JOBINDEX.dat --ncpu=8
+pythonbrew py -p 2.7.3 set_limits.py batch signal_slep.root slep_masses.json $(($LSB_JOBINDEX-1)) limits/results/slep_$LSB_JOBINDEX.dat --ncpu=8 --channels=sf
